fca58a806ae47a79089431b439f71215be2ddbff
nannohk <nannohkw@unb.ca>
2021-11-08 16:43:14 -0400

    Story Extraction from project Gutenberg

---
+++ Main_Script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Welcome to Nathan's Greek Mythology Project"
+
+echo "Obtaining text.... THE ARGONAUTICA "
+
+wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O Story.txt
+
+echo "Text download completed in output file Story.txt yaay"
