d3ee9bd61656c567968c6e1928ff94e9a5206cd8
nannohk <nannohkw@unb.ca>
2021-11-08 22:05:37 -0400

    Using the html version of the file since the txt version wasn't human readable.

--- Main_Script.sh
+++ Main_Script.sh
@@ -4,7 +4,7 @@ echo "Welcome to Nathan's Greek Mythology Project"
 
 echo "Obtaining text.... THE ARGONAUTICA "
 
-wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O Story.txt
+wget https://www.gutenberg.org/cache/epub/830/pg830-images.html.utf8 -O Story.txt
 
 echo "Text download completed in output file Story.txt yaay"
 
