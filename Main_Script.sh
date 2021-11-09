14ef7fbdc12fec9d28c87adf04252e9dc7d2345d
nannohk <nannohkw@unb.ca>
2021-11-09 16:55:17 -0400

    Story obtained in txt form. Next up awk extraction

--- Main_Script.sh
+++ Main_Script.sh
@@ -4,7 +4,9 @@ echo "Welcome to Nathan's Greek Mythology Project"
 
 echo "Obtaining text.... THE ARGONAUTICA "
 
-wget https://www.gutenberg.org/cache/epub/830/pg830-images.html.utf8 -O story.txt
+wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O story.txt.gz
+
+gunzip story.txt.gz
 
 echo "Text download completed in output file story.txt yaay"
 
