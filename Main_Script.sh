cad64d51d576c67ad366c9923a8bd5e97d314d2c
nannohk <nannohkw@unb.ca>
2021-11-08 22:10:27 -0400

    changed S to s

--- Main_Script.sh
+++ Main_Script.sh
@@ -4,9 +4,9 @@ echo "Welcome to Nathan's Greek Mythology Project"
 
 echo "Obtaining text.... THE ARGONAUTICA "
 
-wget https://www.gutenberg.org/cache/epub/830/pg830-images.html.utf8 -O Story.txt
+wget https://www.gutenberg.org/cache/epub/830/pg830-images.html.utf8 -O story.txt
 
-echo "Text download completed in output file Story.txt yaay"
+echo "Text download completed in output file story.txt yaay"
 
 echo "Running Awk Extraction Scrtipt.....storyExtraction.awk"
 ./storyExtraction.awk
