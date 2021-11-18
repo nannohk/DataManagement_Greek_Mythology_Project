ca785835178cf9efb5c54f8006d4fa354c2e5cc0
nannohk <nannohkw@unb.ca>
2021-11-18 19:50:25 -0400

    Story extraction complete.

--- Main_Script.sh
+++ Main_Script.sh
@@ -4,12 +4,15 @@ echo "Welcome to Nathan's Greek Mythology Project"
 
 echo "Obtaining text.... THE ARGONAUTICA "
 
-#wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O story.txt.gz
+wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O story.txt.gz
 
-#gunzip story.txt.gz
+gunzip story.txt.gz
 
 echo "Text download completed in output file story.txt yaay"
 
 echo "Extracting story from Story.txt."
-sed -n '/BOOK I/,/ENDNOTES:/p' story.txt > Story.txt 
+sed -n '/BOOK I/,/BOOK I./p' story.txt > Story.txt 
+
+echo "Cleaning up the directory."
+rm story.txt
 echo "Exiting"
