86cadc01c7801d03af426b07ce782671633b0d0d
nannohk <nannohkw@unb.ca>
2021-11-17 21:00:28 -0400

    Used sed for extraction
    Yet to test it out

--- Main_Script.sh
+++ Main_Script.sh
@@ -8,7 +8,7 @@ wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O Story.txt
 
 echo "Text download completed in output file Story.txt yaay"
 
-echo "Running Awk Extraction Scrtipt.....storyExtraction.awk"
-./storyExtraction.awk
+echo "Extracting story from Story.txt."
+sed -n'/^THE ARGONAUTICA/,/^ENDNOTES:/p' Story.txt > Story.txt 
 
 echo "Exiting"
