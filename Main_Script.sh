b70b3f50555027373b4b08b44f9900837c4fd547
nannohk <nannohkw@unb.ca>
2021-11-24 21:27:01 -0400

    Still low budget name extraction with a few articles and verbs.

--- Main_Script.sh
+++ Main_Script.sh
@@ -4,7 +4,7 @@ echo "Welcome to Nathan's Greek Mythology Project"
 
 echo "Obtaining text.... THE ARGONAUTICA "
 
-#wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O story.txt.gz
+wget https://www.gutenberg.org/cache/epub/830/pg830.txt.utf8.gzip -O story.txt.gz
 
 gunzip story.txt.gz
 
@@ -14,7 +14,9 @@ echo "Extracting story from Story.txt."
 sed -n '/BOOK I/,/BOOK I./p' story.txt > Story.txt 
 
 echo "Cleaning up the directory."
+rm story.txt
+touch names.txt
 
-#awk -f nameExtraction Story.txt
-awk 'match($0, /[A-Z]+[a-z]* /) {print substr($0,RSTART,RLENGTH)}' Story.txt
+awk 'match($0, / [A-Z]+[a-z][a-z][a-z]+ /) {printf substr($0,RSTART,RLENGTH)"\n" > "names.txt"} ' Story.txt
+
 echo "Exiting"
