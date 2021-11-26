a563767d7526f7a1b408d5539a4e717620b25fd5
nannohk <nannohkw@unb.ca>
2021-11-26 16:09:04 -0400

    Name files created. Next up: Obtain the line numbers with those names from the Story

--- Main_Script.sh
+++ Main_Script.sh
@@ -13,10 +13,23 @@ echo "Text download completed in output file story.txt yaay"
 echo "Extracting story from Story.txt."
 sed -n '/BOOK I/,/BOOK I./p' story.txt > Story.txt 
 
-echo "Cleaning up the directory."
-rm story.txt
-touch names.txt
 
-awk 'match($0, / [A-Z][a-z][a-z][a-z]+ /) {printf substr($0,RSTART,RLENGTH)"\n" > "names.txt"} ' Story.txt
+mkdir namesFolder
+cp Story.txt namesFolder/
+
+cd namesFolder
+awk 'match($0, / [A-Z][a-z][a-z][a-z]+ /) {printf substr($0,RSTART,RLENGTH)"\n"} ' Story.txt | xargs touch 
+
+#awk 'match($0, / [A-Z][a-z][a-z][a-z]+ /) {printf substr($0,RSTART,RLENGTH)"\n"} ' Story.txt | sed 's/^\$PWD/namesFolder\///g' | xargs touch 
+#awk '{print $0}' names.txt | touch $pwd/namesFolder/
+
+echo "Cleaning up the directories."
+rm story.txt
+rm Story.txt
+# Remove  64 non name words
+rm After Alone Away Banded Bear Beginning Bitter Come Does Earth Earthborn Even Fair First From Grant   
+rm  Meantime  More Moreover Most Mount Never Next Night Perish Likewise Long Love Loves
+rm Quickly Quivering Sacred Shining Speak Still Such Surely Them Thus Then Thence 
+rm There Therefore Thereupon This Thou Twelve What When Wherefore Whether Whither Will Wilt Would Zone Poor Here
 
 echo "Exiting"
