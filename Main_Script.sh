dfe9a0af013ea912951ac0a0e843ab1cdc748809
nannohk <nannohkw@unb.ca>
2021-11-27 23:33:58 -0400

    Character names filtered. Insertion files created and populated with Mongo instrctions.

--- Main_Script.sh
+++ Main_Script.sh
@@ -26,10 +26,38 @@ awk 'match($0, / [A-Z][a-z][a-z][a-z]+ /) {printf substr($0,RSTART,RLENGTH)"\n"}
 echo "Cleaning up the directories."
 rm story.txt
 rm Story.txt
+
 # Remove  64 non name words
 rm After Alone Away Banded Bear Beginning Bitter Come Does Earth Earthborn Even Fair First From Grant   
-rm  Meantime  More Moreover Most Mount Never Next Night Perish Likewise Long Love Loves
-rm Quickly Quivering Sacred Shining Speak Still Such Surely Them Thus Then Thence 
+rm  Meantime  More Moreover Most Mount Never Next Night Perish Likewise Long Love Loves These Nevertheless
+rm Quickly Quivering Sacred Shining Speak Still Such Surely Them Thus Then Thence With Straightway King
 rm There Therefore Thereupon This Thou Twelve What When Wherefore Whether Whither Will Wilt Would Zone Poor Here
 
+
+echo "Setting up Mongo insert commands"
+for filename in *;
+do 
+    #Obtain the lines with the particular name
+    #Add list of lines with the particular name
+
+    #Append instructions to files
+    echo "db.GreekCharacters.insert({\"name\":\"$filename\",\"lines\":\"listOfLines\"})" >> $filename;
+done
+
+echo "Creating greek character database"
+mongo GreekCharacters
+
+echo "Running Insert commands"
+chmod +x *
+
+for file in *;
+do 
+    ./$file
+done
+
+# echo "Creating final nameList containing all names for the Database"
+# ls >> nameList.txt
+
+# echo "Insert names from nameList file"
+# ./insertNames.sh
 echo "Exiting"
