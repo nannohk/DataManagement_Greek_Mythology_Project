07d4a651be55400453e3db1fae4d0655c48e8b81
nannohk <nannohkw@unb.ca>
2021-11-29 14:56:13 -0400

    Mongo insert instructions successfully added to the necessary files

--- Main_Script.sh
+++ Main_Script.sh
@@ -48,14 +48,14 @@ echo "Setting up Mongo insert commands"
 for filename in ${fileNames[@]};
 do 
     #Obtain the lines with the particular name
-    grep -n ${filename} $masterDirectory/story.txt | awk -v "name=$filename" ' BEGIN { FS = OFS = ":"} {printf("db.GreekCharacters.insert({\"lines\":"$1"})\n") > $name}'
-    echo "----------------------"
+   grep -n ${filename} $masterDirectory/story.txt | awk ' BEGIN { FS= OFS = ":"} {printf("db.GreekCharacters.insert({\"lines\":"$1"})\n")}'> $filename
+   # echo "----------------------"
     
     #Add list of lines with the particular name
 
     #Append instructions to files
     #echo "db.GreekCharacters.insert({\"name\":\"$filename\",\"lines\":\"listOfLines\"})" >> $filename;
-   # echo "db.GreekCharacters.insert({\"name\":\"$filename\"})" >> $filename;
+    echo "db.GreekCharacters.insert({\"name\":\"$filename\"})" >> $filename;
 
 done
 
